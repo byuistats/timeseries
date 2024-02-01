2e951751362a79b69f40671587520901e8da1de5
Craig J <johnsonc@byui.edu>
2024-02-01 13:34:57 -0700

    briggs

---
+++ data/briggs_spring_flow/read_and_clean_briggs_spring_flow_data.R
@@ -0,0 +1,38 @@
+# Read and clean spring flow data
+
+briggs <- rio::import("data/briggs_spring.parquet") |>
+  mutate(date2 = ymd_hm(date)) |>
+  mutate(diff = date2 - lag(date2)) |>
+  mutate(minute = minute(date2))
+briggs %>% head
+briggs %>% tail
+briggs %>% View
+briggs %>%
+  ggplot(aes(x = date, y = as.numeric(diff))) +
+  geom_line()
+
+briggs %>% group_by(minute) %>% summarize(n = n()) %>% View
+briggs %>% filter(!(minute %in% c(0,15,30,45))) %>% arrange(date2)
+briggs %>% filter(!(minute %in% c(0,15,30,45))) %>% View
+  summarize(min = min(date2), max = max(date2))
+
+
+briggs %>%
+  mutate(round_time = round_date(date2, unit = "hour")) %>%
+  mutate(diff = date2 - round_time) %>%
+  filter(!(minute %in% c(15,30,45))) %>%
+  arrange(round_time, abs(diff)) %>%
+  filter(round_time != lag(round_time)) %>%
+  filter(round_time >= mdy("10/1/1995")) %>%
+  filter(round_time >= mdy("10/1/2006")) %>%
+  filter(round_time >= mdy("10/1/2007")) %>%
+  # filter(round_time <= mdy("10/1/2008")) %>%
+  as_tsibble(index = round_time) %>%
+  fill_gaps() %>%
+  autoplot(.vars = flow)
+
+
+celeb_bdays %>%
+  mutate(
+    round_month = round_date(birthday, unit = "month")
+  )
