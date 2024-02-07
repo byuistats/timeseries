8f3052e913076a7df75902c808f08c6a154a6516
Davie <107007169+daviethedeveloper@users.noreply.github.com>
2024-02-07 11:33:46 -0700

    Update Simulation_In_R.R

--- ShinyApps/4_Simluation/Simulation_In_R.R
+++ ShinyApps/4_Simluation/Simulation_In_R.R
@@ -7,18 +7,19 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(ggokabeito)
+library(fpp3) 
 library(tsibble)
 
 ui <- fluidPage(
   titlePanel("Bivariate Normal Distribution Simulator"),
-
+  
   sidebarLayout(
     sidebarPanel(
       sliderInput("sigma", "Standard Deviation", min = 0, max = 10, value = 1),
-      sliderInput("n_points", "Number of Points", min = 10, max = 500, value = 100, step = 10),
+      sliderInput("n_points", "Number of Points", min = 10, max = 500, value = 100, step = 10), 
       actionButton("go", "Simulate!")
     ),
-
+    
     mainPanel(
       plotOutput("plot"),
       plotOutput("additional_plot")
@@ -26,41 +27,60 @@ ui <- fluidPage(
   )
 )
 
+
+
 server <- function(input, output, session) {
   sim_data <- eventReactive(input$go, {
     set.seed(1)
-
-
+    
     wd <- tibble(
       seq = seq_len(input$n_points),
       values = rnorm(input$n_points, sd = input$sigma)
     )
-
-    # ACF plot data
-    acf_plot <- tsibble(
-      seq = seq_len(nrow(wd)),
-      values = wd$values,
-      index = seq
-    ) |> acf(values) |> autoplot()
-
-    list(sim_data = wd, acf_plot = acf_plot)
+    
+    wd_tsibble <- wd %>%
+      as_tsibble(index = seq)
+    
+    acf_object <- ACF(wd_tsibble)
+    
+    print(acf_object)
+    
+    list(sim_data = wd, acf_object = acf_object)
   })
-
+  
   output$plot <- renderPlot({
     df <- sim_data()$sim_data
-
-
+    
     ggplot(df, aes(x = seq, y = values)) +
       geom_line() +
       labs(title = "Bivariate Normal Distribution Simulator",
            x = "x",
            y = "y")
   })
-
+  
   output$acf_plot <- renderPlot({
-    sim_data()$acf_plot
+    # Get the ACF object from sim_data
+    acf_object <- sim_data()$acf_object
+    
+    # Create ACF plot
+    acf_plot <- ggplot(data = acf_object, aes(x = lag, y = acf)) +
+      geom_bar(stat = "identity") +
+      labs(title = "Autocorrelation Function Plot",
+           x = "Lag",
+           y = "Autocorrelation Coefficient")
+    
+    # Print ACF plot object
+    print(acf_plot)
+    
+    # Return ACF plot
+    acf_plot
   })
+  
 }
+
+
+
+
 
 
 shinyApp(ui, server)
