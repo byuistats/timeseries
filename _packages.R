7bc393c38189e543d57ffe76483e21993b466c1f
Craig J <johnsonc@byui.edu>
2024-03-13 22:05:20 -0600

    More edits.

---
+++ _packages.R
@@ -0,0 +1,20 @@
+# Load packages ----
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(MASS, # for MVNorm - load before tidyverse
+               tidyverse, kableExtra,
+               tsibble, fable,
+               feasts, tsibbledata,
+               fable.prophet,
+               patchwork,
+               ggthemes,
+               see,   # okabeito color scheme
+               ggokabeito, # colorblind palette
+               stringr, # string manipulation
+               lubridate, # date manipulation
+               rio, # for easy i/o
+               tidyquant,
+               gt, # grammar of tables, for LaTeX in tables
+               quarto, # For LaTeX mode results = 'asis'
+               plotly # For interactive figures
+)
