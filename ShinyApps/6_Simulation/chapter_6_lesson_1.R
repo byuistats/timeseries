84001fddc67802c0c1cb9b173961ddd5fa1f8971
Tyson Brost <tyson.brost@gmail.com>
2024-03-22 16:15:53 -0600

    edits

--- ShinyApps/6_Simulation/chapter_6_lesson_1.R
+++ ShinyApps/6_Simulation/chapter_6_lesson_1.R
@@ -18,17 +18,35 @@ library(ggfortify)
 
 # Define the UI
 ui <- fluidPage(
+  tags$head(
+    tags$link(rel="stylesheet",
+              href="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.css",
+              integrity="sha384-dbVIfZGuN1Yq7/1Ocstc1lUEm+AT+/rCkibIcC/OmWo5f0EA48Vf8CytHzGrSwbQ",
+              crossorigin="anonymous"),
+    HTML('<script defer src="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.js" integrity="sha384-2BKqo+exmr9su6dir+qCw08N2ZKRucY4PrGQPPWU1A7FtlCGjmEGFqXCv5nyM5Ij" crossorigin="anonymous"></script>'),
+    HTML('<script defer src="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/contrib/auto-render.min.js" integrity="sha384-kWPLUVMOks5AQFrykwIup5lo0m3iMkkHrD0uJ4H5cjeGihAutqP0yW0J6dpFiVkI" crossorigin="anonymous"></script>'),
+    HTML('
+    <script>
+      document.addEventListener("DOMContentLoaded", function(){
+        renderMathInElement(document.body, {
+          delimiters: [{left: "$", right: "$", display: false}]
+        });
+      })
+    </script>')
+  ),
   withMathJax(),
   titlePanel("MA(3) Process Simulation"),
   fluidRow(
-    column(3, numericInput("n", label = "$$ \\text{Numerical Value (n)} $$", value = 1000)),
-    column(3, numericInput("beta1", label = "$$\\beta_1$$", value = 0.7)),
-    column(3, numericInput("beta2", label = "$$\\beta_2$$", value = 0.5)),
-    column(3, numericInput("beta3", label = "$$\\beta_3$$", value = 0.2)),
+    column(3, numericInput("n", label = "$n$", value = 1000)),
+    column(3, numericInput("beta1", label = "$\\beta_1$", value = 0.7)),
+    column(3, numericInput("beta2", label = "$\\beta_2$", value = 0.5)),
+    column(3, numericInput("beta3", label = "$\\beta_3$", value = 0.2)),
   ),
   fluidRow(
     column(4, offset = 5, actionButton("go", "Simulate!")),
-    column(12, h4(""))
+    column(4, h4("")),
+    column(12, div(style = "display: flex; justify-content: center; align-items: center;", uiOutput("formula"))),
+    column(4, h4(""))
   ),
   fluidRow(
     column(6, plotOutput("plot1")),
@@ -83,7 +101,7 @@ server <- function(input, output) {
     }
 
     acf_dat <- tibble(
-      order = 0:10,
+      order = 0:15,
       betas = list(c(1, beta1, beta2, beta3)),
       rho.k = map2_dbl(order, betas, ~rho(.x, .y))
     )
@@ -94,7 +112,8 @@ server <- function(input, output) {
       labs(y = expression(rho[k]), x = "lag k") +
       theme_bw() +
       theme(plot.title = element_text(hjust = 0.5)) +
-      ggtitle("Theoretical ACF for the Simulated MA(3) Process")
+      ggtitle("Theoretical ACF for the Simulated MA(3) Process")+
+      coord_cartesian(xlim = c(0, 15))
   })
 
   output$plot2 <- renderPlot({
@@ -111,11 +130,19 @@ server <- function(input, output) {
     # ACF plot data
     acf_plot_data <- simulate_data() |>
       pull(x) |>
-      acf() |>
+      acf(lag.max = 15) |>
       autoplot(main= "Observed ACF from Simulated MA(3) Process")
 
     # Plot ACF using ggplot
     acf_plot_data
+  })
+
+  output$formula <- renderUI({
+    req(simulate_data())
+    tagList(
+      withMathJax(paste0("$x_t = w_t + ",input$beta1," w_{t-1} +", input$beta2, "w_{t-2} + ",input$beta3,"w_{t-3}$")),
+      tags$script('renderMathInElement(document.getElementById("formula"), {delimiters: [{left: "$", right: "$", display: false}]});')
+    )
   })
 }
 
