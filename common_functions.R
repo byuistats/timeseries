3c79766d2790ac62f155fbada663b1f7b2941cb2
Tyson Brost <tyson.brost@gmail.com>
2024-03-29 16:18:16 -0600

    better explains

--- common_functions.R
+++ common_functions.R
@@ -1,26 +1,45 @@
 # Load packages ----
 
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(MASS, # MVNorm, loaded before tidyverse so it doesn't overwrite dplyr::select()
-               nlme,
-               tidymodels,    # for GLS
-               tidyverse, kableExtra,
-               tsibble, fable,
-               feasts, tsibbledata,
-               fable.prophet,
-               patchwork,
-               ggthemes,
-               see,   # okabeito color scheme
-               ggokabeito, # colorblind palette
-               # stringr, # string manipulation
-               # lubridate, # date manipulation
-               rio, # for easy i/o
-               tidyquant,
-               gt, # grammar of tables, for LaTeX in tables
-               quarto, # For LaTeX mode results = 'asis'
-               plotly, # For interactive figures
-               multilevelmod, # for GLS
-               broom.mixed    # for GLS
+pacman::p_load(
+  # Interactive plots
+  plotly, # Interactive visualizations, loaded before tidyverse so it overwrite dplyr::select(). Note: High conflict Potential
+
+  # Core packages
+  MASS, # MVNorm, loaded before tidyverse so it doesn't overwrite dplyr::select()
+  tidyverse, # This will also load the dependencies; dplyr, readr, stringr, tibble, tidyr, purrr, forcats, gglot2, & lubridate
+
+  # Data manipulation
+  tsibble, # Tidyverse Temporal data
+  tsibbledata, # Sample Tsibble datasets
+
+
+  # Statistical modeling (GLS - Chpt 6-7)
+  nlme, # loaded before feasts to avoid ACF() conflict
+  tidymodels, # for GLS, This will also load the dependencies; broom, rsample, dials, tune, infer, workflows, modeldata, workflowsets, parsnip, yardstick, & recipies. Note: High conflict Potential
+  multilevelmod, # for GLS
+  broom.mixed, # for GLS
+
+  # TS modeling and forecasting
+  fable,# Forecasting Models for Tidy Time Series, Note: High conflict Potential
+  feasts, # collection of features, decomposition methods, statistical summaries and graphics for tsibble data, Loaded after nlme to avoid ACF() conflict
+  fable.prophet, # Converts prophet (forecasting) package for fable workflow
+
+  # Data exploration & visualization
+  patchwork, # Multiple plot outputs
+  ggthemes, # Plot styling
+  see,  # okabeito color scheme
+  ggokabeito,  # colorblind palette
+
+
+  # Reporting & output
+  kableExtra, # Create nice-looking tables from data.frames
+  rio, # Easy import/export of data between R and other software
+  gt, # Grammar of Tables for advanced table creation
+  quarto, # For generating reports in LaTeX format
+
+  # Additional packages
+  tidyquant # Quantitative analysis tools using tidyverse principles, This will also load the dependencies; PerformanceAnalytics, xts, & zoo. Important Masks: ‘package:base’: as.Date, as.Date.numeric. Note: High conflict Potential
 )
 ################# WARNING: DO NOT USE mosaic. IT MESSES UP THE DECOMPOSITION. #need to test loading this in the front, might be ok
 
